464c7d51128af153c5220ded094b08169c38e78e
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2024-08-12 09:46:01 -0400

    mvp script, hopefully

---
+++ analyses/gpu-cellassign-testing/small-cellassign.py
@@ -0,0 +1,30 @@
+import anndata as adata
+import pandas as pd
+import scvi
+from scvi.external import CellAssign
+
+
+scvi.settings.seed = 2024
+
+# read in marker gene reference
+ref_matrix = pd.read_csv(
+    "full-brain-reference.tsv", sep="\t", index_col="ensembl_gene_id"
+)
+
+# read in anndata
+adata = adata.read_h5ad("SCPCS000001_processed_rna.h5ad")
+
+# subset anndata to contain only genes in the reference file
+shared_genes = list(set(ref_matrix.index) & set(annotated_adata.var_names))
+subset_adata = adata[:, shared_genes].copy()
+subset_adata.X = subset_adata.X.tocsr()
+
+# add size factor to subset adata (calculated from full data)
+lib_size = adata.X.sum(1)
+subset_adata.obs["size_factor"] = lib_size / np.mean(lib_size)
+
+# CellAssign inference
+scvi.external.CellAssign.setup_anndata(subset_adata, size_factor_key="size_factor")
+model = CellAssign(subset_adata, ref_matrix)
+model.train(accelerator="gpu")
+predictions = model.predict()
